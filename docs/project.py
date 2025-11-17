dae82dcccc598169a6796a47028602ceab389da5
Andrew Back <andrew@abopen.com>
2025-11-17 21:10:59 +0000

    Docs: update Sphinx config.

--- docs/project.py
+++ docs/project.py
@@ -3,7 +3,7 @@
 project = 'LimePPSDO'
 copyright = '2025 Lime Microsystems'
 author = 'Lime Microsystems'
-release = 'v0'
+release = 'latest'
 
 # These are used for the "Edit on GitHub" links.
 # github_repo_path should be set to the branch + path to the docs.
@@ -16,5 +16,20 @@ github_repo_path= 'master/docs/'
 # This can be overridden using the ".. code-block::" directive.
 highlight_language = 'python'
 
+# Intersphinx mapping
+# For MyriadRF internal projects list the project slug only.
+# For external projects specify the reference and the full URL.
+# To minimise build time only include projects that are referenced!
+intersphinx_internal = [
+    'limedfb',
+    'limesdr-gw',
+]
+intersphinx_external = {
+    # 'numpy': 'https://numpy.org/doc/stable/',
+}
+
 # Set to True if the project is archived.
 archived = False
+
+# When True internal intersphinx targets point at stage.myriadrf.org.
+staging = True
